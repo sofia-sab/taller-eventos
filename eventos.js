230f9be5b176ab8b6a018340c38e9f39784ec3c6
sofia-sab <sofiasaboredo@gmail.com>
2024-08-19 23:18:36 -0300

    Solucion click

--- eventos.js
+++ eventos.js
@@ -1,10 +1,24 @@
-function divClick(){
-    alert("Hola! Soy el div")
+function divClick(e){
+    alert("Hola! Soy el div");
+    e.stopPropagation();
+    
+}
+
+function btnClick(){
+    alert("Hola!");
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById("div").addEventListener('click', ()=>{
         divClick();
+        
+    }
+)})
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById("btn").addEventListener('click', (e)=>{
+        e.stopPropagation();
+        btnClick();
        
     }
 )})
